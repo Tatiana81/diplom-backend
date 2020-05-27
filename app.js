0774fa943c979332da9862d1d54a833a9376df43
Tatiana <kriista@mail.ru>
2020-05-28 00:47:16 +0300

    app added

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = require('./routes/index');
+
+const { PORT, DATABASE_URL } = require('./config.js');
+
+const app = express();
+
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(router);
+
+app.listen(PORT);
