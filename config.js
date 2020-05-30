5b34e42a975a60aa8836dfd282e429770aec352b
Tatiana <kriista@mail.ru>
2020-05-31 00:16:15 +0300

    to create pull request

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ module.exports.DB_PARAMS = {
 module.exports.messages = {
   wrongUserPassword: 'Неправильный пользователь или пароль',
   wrongId: 'Нет документа с таким id',
-  permissionDenied: 'У Вас недостаточно прав на совершение операции',
+  permissionDenied: 'Нельзя удалять чужие статьи',
   userExists: 'Пользователь уже зарегистрирован. Войти в систему?',
   authorError: 'Необходима авторизация',
   resourceNotFound: 'Запрашиваемый ресурс не найден',
   serverError: 'На сервере произошла ошибка',
-  noArticle: 'На сервере нет Ваших статей',
+  noArticle: 'Вы не сохранили ни одной статьи',
 };
